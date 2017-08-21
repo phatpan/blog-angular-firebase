4aa6b928950fe37747663590b5fe978eacc4443d
phatcharaphan <phat.pan@gmail.com>
2017-08-21 22:54:11 +0700

    add layout login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire4/database';
-import { AngularFireAuth } from 'angularfire4/auth';
-import { Observable } from 'rxjs/Observable';
-import * as firebase from 'firebase/app';
+import { AngularFireAuth } from 'angularfire4/auth'
+import { Observable } from 'rxjs/Observable'
+import * as firebase from 'firebase/app'
 
 @Component({
   selector: 'app-root',
@@ -10,20 +10,27 @@ import * as firebase from 'firebase/app';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
-  
   user: Observable<firebase.User>;
   items: FirebaseListObservable<any[]>;
   msgVal: string = '';
 
-  constructor(public afAuth: AngularFireAuth, public af: AngularFireDatabase) {
+  constructor(public afAuth: AngularFireAuth, public af: AngularFireDatabase){
     this.items = af.list('/messages', {
       query: {
         limitToLast: 50
       }
     });
-
     this.user = this.afAuth.authState;
+  }
 
+  login(){
+    this.afAuth.auth.signInAnonymously();
+  }
+  logout(){
+    this.afAuth.auth.signOut();
+  }
+  Send(desc: string){
+    this.items.push({message: desc});
+    this.msgVal = '';
   }
 }
